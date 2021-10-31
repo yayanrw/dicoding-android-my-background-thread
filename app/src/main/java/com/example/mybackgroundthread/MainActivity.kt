6d87e75b1a7408302b369c4af1b352d3e106085d
yayan-msglow <yayanrahmat@msglowid.com>
2021-11-01 05:26:29 +0700

    add coroutines

--- app/src/main/java/com/example/mybackgroundthread/MainActivity.kt
+++ app/src/main/java/com/example/mybackgroundthread/MainActivity.kt
@@ -7,6 +7,11 @@ import android.os.Looper
 import android.os.Looper.*
 import android.widget.Button
 import android.widget.TextView
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
@@ -21,23 +26,57 @@ class MainActivity : AppCompatActivity() {
         val handler = Handler(Looper.getMainLooper())
 
         btnStart.setOnClickListener {
-            executor.execute {
-                try {
-                    //simulate process in background thread
-                    for (i in 0..10) {
-                        Thread.sleep(500)
-                        val percentage = i * 10
-                        handler.post {
-                            //update ui in main thread
-                            if (percentage == 100) {
-                                tvStatus.setText(R.string.task_completed)
-                            } else {
-                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
-                            }
+//            no handler
+//            try {
+//                //simulate process compressing
+//                for (i in 0..10) {
+//                    Thread.sleep(500)
+//                    val percentage = i * 10
+//                    if (percentage == 100) {
+//                        tvStatus.setText(R.string.task_completed)
+//                    } else {
+//                        tvStatus.text = String.format(getString(R.string.compressing), percentage)
+//                    }
+//                }
+//            } catch (e: InterruptedException) {
+//                e.printStackTrace()
+//            }
+
+//            withhandler
+//            executor.execute {
+//                try {
+//                    //simulate process in background thread
+//                    for (i in 0..10) {
+//                        Thread.sleep(500)
+//                        val percentage = i * 10
+//                        handler.post {
+//                            //update ui in main thread
+//                            if (percentage == 100) {
+//                                tvStatus.setText(R.string.task_completed)
+//                            } else {
+//                                tvStatus.text = String.format(getString(R.string.compressing), percentage)
+//                            }
+//                        }
+//                    }
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+//            }
+
+//            with coroutines
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10) {
+                    delay(500)
+                    val percentage = i * 10
+                    withContext(Dispatchers.Main) {
+                        //update ui in main thread
+                        if (percentage == 100) {
+                            tvStatus.setText(R.string.task_completed)
+                        } else {
+                            tvStatus.text = String.format(getString(R.string.compressing), percentage)
                         }
                     }
-                } catch (e: InterruptedException) {
-                    e.printStackTrace()
                 }
             }
         }
